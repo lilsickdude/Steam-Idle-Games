5520271dbc4e7f8e3700601993c606c4d299576d
lilsickdude <64420285+lilsickdude@users.noreply.github.com>
2020-06-18 22:27:52 +0200

    Version 1.1
    - Adding "Last Idle Game" function
    - Now the window is not resizable
    - Changing the ".bat" file TITLE name

--- Idle Games Lite/main.py
+++ Idle Games Lite/main.py
@@ -1,9 +1,13 @@
+# Version 1.1
+
 import os
 import tkinter
 from tkinter import *
 
+# Main Menu
 root = Tk()
-root.title('Idle Games LITE')
+root.title('Idle Games Lite')
+root.resizable(width=False, height=False)
 Label(text='Enter ID Game:').pack(side=TOP,padx=10,pady=10)
 GameID = Entry(root, width=100)
 GameID.pack(side=TOP,padx=10,pady=10)
@@ -15,7 +19,10 @@ def idle():
     ideling = open("StartIdling.bat","w")
     #   Write in .bat File
     ideling.write("@echo off\n")
-    ideling.write("TITLE Execution IdleMode: ON You can close this\n")
+    ideling.write("TITLE You are on IdleMode, don't close this windows until the game idle doesn't run\n")
+    ideling.write("color 0C\n")
+    ideling.write("echo Hi, please don't close this window until the idle game that you have choose is not running \n")
+    ideling.write("echo Steam should be already run\n")
     ideling.write("steam-idle.exe ")
     ideling.write(GameIDSel)
 
@@ -28,9 +35,14 @@ def idle():
     print("Indle Start!")
     print("Game ID: ", GameIDSel)
 
+def last():
+    print("You are running last idle game")
+    os.system("start StartIdling.bat")
+
     
 
 Button(root, text='Idle!', command=idle).pack(side=LEFT)
 Button(root, text='Exit', command= root.destroy).pack(side= RIGHT)
+Button(root, text='Idle Last Game', command =last).pack(side=BOTTOM)
 
 root.mainloop()
